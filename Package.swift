e2858fa7d335ab55a1cdda7de09af2e96195b7f1
jenkins <jenkins@edna.ru>
2022-03-03 16:11:33 +0300

    Release 1.2.1

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "EDNAPushXNE",
-            url: "https://github.com/edna-io/push-x-ne-ios/releases/download/1.2.0/edna-push-xne-1.2.0.zip",
-            checksum: "1c62aca57ca2b1f6c3c33e23195c7078da698ee00f6855de4bbac7a802ff3186"
+            url: "https://github.com/edna-io/push-x-ne-ios/releases/download/1.2.1/edna-push-xne-1.2.1.zip",
+            checksum: "8d9ad46038b61ca40a63b185016a0e8b47d6100fb1d4549dd5ae7ba1866e4052"
         ),
     ]
 )
